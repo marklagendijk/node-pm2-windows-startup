2a3b0b6f2aaecbeef78ebbcfbaee7c6900e1c6c3
Mark Lagendijk <mark@lagendijk.info>
2015-07-14 00:28:34 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node-pm2-windows-startup
-Utility to make PM2 automatically start on Windows startup
+Utility to make PM2 automatically resurrect on Windows startup. The utility achieves this by adding the right registry entry, using [start-on-windows-boot](https://github.com/marklagendijk/node-start-on-windows-boot).
+
+## Installation and usage
+1. `npm install pm2-windows-startup -g`
+2. `pm2-startup install`
---
+++ index.js
@@ -0,0 +1,44 @@
+var startOnBoot = require('start-on-windows-boot');
+var argv = require('yargs')
+    .usage('Usage: pm2-startup <command>')
+    .command('install', 'Adds a registry entry which resurrects PM2 on startup.')
+    .command('uninstall', 'Removes the registry entry which resurrects PM2 on startup.')
+    .demand(1)
+    .argv
+    ._;
+
+var applicationName = 'PM2';
+var applicationCommand = 'wscript.exe "' + __dirname + '\\invisible.vbs" "' + __dirname + '\\pm2_resurrect.cmd"';
+
+switch (argv[0]) {
+    case 'install':
+        enablePm2Startup();
+        break;
+
+    case 'uninstall':
+        removePm2Startup();
+        break;
+}
+
+
+function enablePm2Startup() {
+    startOnBoot.enableAutoStart(applicationName, applicationCommand, function (error) {
+        if (error) {
+            console.log('Error while trying to add PM2 startup registry entry: ' + error);
+        }
+        else {
+            console.log('Successfully added PM2 startup registry entry.');
+        }
+    });
+}
+
+function removePm2Startup() {
+    startOnBoot.disableAutoStart(applicationName, function (error) {
+        if (error) {
+            console.log('Error while trying to remove PM2 startup registry entry: ' + error);
+        }
+        else {
+            console.log('Successfully removed PM2 startup registry entry.');
+        }
+    });
+}
